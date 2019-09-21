4992da9d06d70083aa8eb37e997614da2552a430
pynickle <47242104+code-nick-python@users.noreply.github.com>
2019-09-21 20:51:00 +0800

    fix

--- instance/convert.py
+++ instance/convert.py
@@ -2,7 +2,7 @@ import os
 import pypandoc
 
 def convert():
-    for i in range(1, len(os.listdir("/instance/articles")) + 1):
-        output = pypandoc.convert_file("/instance/articles/" + str(i) + ".md", "html")
-        with open("/templates/articles/" + str(i) + ".html", "w", encoding="utf-8") as f:
+    for i in range(1, len(os.listdir(os.getcwd() + "/instance/articles")) + 1):
+        output = pypandoc.convert_file(os.getcwd() + "/instance/articles/" + str(i) + ".md", "html")
+        with open(os.getcwd() + "/templates/articles/" + str(i) + ".html", "w", encoding="utf-8") as f:
             f.write(output)
